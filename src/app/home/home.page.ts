098f95e2c94451656447c72e683d19ec90e2602c
unknown <trixkal@gmail.com>
2020-04-09 12:34:30 -0600

    add event logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,129 +1,125 @@
-import { Component } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-
-  eventSource;
-  viewTitle;
-
-  isToday: boolean;
-  calendar = {
+export class HomePage implements OnInit {
+ 
+    collapseCard = true;
+    event = {
+      title: '',
+      desc: '',
+      startTime: '',
+      endTime: '',
+      allDay: false
+    };
+   
+    minDate = new Date().toISOString();
+   
+    eventSource = [];
+    viewTitle;
+   
+    calendar = {
       mode: 'month',
       currentDate: new Date(),
-      dateFormatter: {
-          formatMonthViewDay(date: Date) {
-              return date.getDate().toString();
-          },
-          formatMonthViewDayHeader(date: Date) {
-              return 'MonMH';
-          },
-          formatMonthViewTitle(date: Date) {
-              return 'testMT';
-          },
-          formatWeekViewDayHeader(date: Date) {
-              return 'MonWH';
-          },
-          formatWeekViewTitle(date: Date) {
-              return 'testWT';
-          },
-          formatWeekViewHourColumn(date: Date) {
-              return 'testWH';
-          },
-          formatDayViewHourColumn(date: Date) {
-              return 'testDH';
-          },
-          formatDayViewTitle(date: Date) {
-              return 'testDT';
-          }
+    };
+   
+    @ViewChild(CalendarComponent) myCal: CalendarComponent;
+   
+    constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
+   
+    ngOnInit() {
+      this.resetEvent();
+    }
+   
+    resetEvent() {
+      this.event = {
+        title: '',
+        desc: '',
+        startTime: new Date().toISOString(),
+        endTime: new Date().toISOString(),
+        allDay: false
+      };
+    }
+   
+    // Create the right event format and reload source
+    addEvent() {
+      let eventCopy = {
+        title: this.event.title,
+        startTime:  new Date(this.event.startTime),
+        endTime: new Date(this.event.endTime),
+        allDay: this.event.allDay,
+        desc: this.event.desc
       }
-  };
+   
+      if (eventCopy.allDay) {
+        let start = eventCopy.startTime;
+        let end = eventCopy.endTime;
+   
+        eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+        eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+      }
+   
+      this.eventSource.push(eventCopy);
+      this.myCal.loadEvents();
+      this.resetEvent();
+    }
 
-  constructor() {
 
+
+// Change current month/week/day
+next() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slideNext();
   }
-
-  loadEvents() {
-      this.eventSource = this.createRandomEvents();
+   
+  back() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slidePrev();
   }
-
-  onViewTitleChanged(title) {
-      this.viewTitle = title;
-  }
-
-  onEventSelected(event) {
-      console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
-  }
-
+   
+  // Change between month/week/day
   changeMode(mode) {
-      this.calendar.mode = mode;
+    this.calendar.mode = mode;
   }
-
+   
+  // Focus today
   today() {
-      this.calendar.currentDate = new Date();
-      this.isToday = true;
+    this.calendar.currentDate = new Date();
   }
-
+   
+  // Selected date reange and hence title changed
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+   
+  // Calendar event was clicked
+  async onEventSelected(event) {
+    // Use Angular date pipe for conversion
+    let start = formatDate(event.startTime, 'medium', this.locale);
+    let end = formatDate(event.endTime, 'medium', this.locale);
+   
+    
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: 'From: ' + start + '<br><br>To: ' + end,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+   
+  // Time slot was clicked
   onTimeSelected(ev) {
-      console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
-          (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    let selected = new Date(ev.selectedTime);
+    this.event.startTime = selected.toISOString();
+    selected.setHours(selected.getHours() + 1);
+    this.event.endTime = (selected.toISOString());
   }
 
-  onCurrentDateChanged(event: Date) {
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-      event.setHours(0, 0, 0, 0);
-      this.isToday = today.getTime() === event.getTime();
   }
-
-  createRandomEvents() {
-      const events = [];
-      for (let i = 0; i < 50; i += 1) {
-          const date = new Date();
-          const eventType = Math.floor(Math.random() * 2);
-          const startDay = Math.floor(Math.random() * 90) - 45;
-          let endDay = Math.floor(Math.random() * 2) + startDay;
-          let startTime;
-          let endTime;
-          if (eventType === 0) {
-              startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
-              if (endDay === startDay) {
-                  endDay += 1;
-              }
-              endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
-              events.push({
-                  title: 'All Day - ' + i,
-                  startTime,
-                  endTime,
-                  allDay: true
-              });
-          } else {
-              const startMinute = Math.floor(Math.random() * 24 * 60);
-              const endMinute = Math.floor(Math.random() * 180) + startMinute;
-              startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
-              endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
-              events.push({
-                  title: 'Event - ' + i,
-                  startTime,
-                  endTime,
-                  allDay: false
-              });
-          }
-      }
-      return events;
-  }
-
-  onRangeChanged(ev) {
-      console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
-  }
-
-  markDisabled = (date: Date) => {
-      const current = new Date();
-      current.setHours(0, 0, 0);
-      return date < current;
-  }
-
-}
