15f7a98223088881473dfdc0464351a95deef66f
unknown <trixkal@gmail.com>
2020-04-09 13:12:44 -0600

    tslint

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,7 @@ import { formatDate } from '@angular/common';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
- 
+
     collapseCard = true;
     event = {
       title: '',
@@ -18,25 +18,25 @@ export class HomePage implements OnInit {
       endTime: '',
       allDay: false
     };
-   
+
     minDate = new Date().toISOString();
-   
+
     eventSource = [];
     viewTitle;
-   
+
     calendar = {
       mode: 'month',
       currentDate: new Date(),
     };
-   
+
     @ViewChild(CalendarComponent) myCal: CalendarComponent;
-   
+
     constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
-   
+
     ngOnInit() {
       this.resetEvent();
     }
-   
+
     resetEvent() {
       this.event = {
         title: '',
@@ -46,25 +46,25 @@ export class HomePage implements OnInit {
         allDay: false
       };
     }
-   
+
     // Create the right event format and reload source
     addEvent() {
-      let eventCopy = {
+      const eventCopy = {
         title: this.event.title,
         startTime:  new Date(this.event.startTime),
         endTime: new Date(this.event.endTime),
         allDay: this.event.allDay,
         desc: this.event.desc
-      }
-   
+      };
+
       if (eventCopy.allDay) {
-        let start = eventCopy.startTime;
-        let end = eventCopy.endTime;
-   
+        const start = eventCopy.startTime;
+        const end = eventCopy.endTime;
+
         eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
         eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
       }
-   
+
       this.eventSource.push(eventCopy);
       this.myCal.loadEvents();
       this.resetEvent();
@@ -74,37 +74,39 @@ export class HomePage implements OnInit {
 
 // Change current month/week/day
 next() {
-    var swiper = document.querySelector('.swiper-container')['swiper'];
+    // tslint:disable-next-line: no-string-literal
+    const swiper = document.querySelector('.swiper-container')['swiper'];
     swiper.slideNext();
   }
-   
+
   back() {
-    var swiper = document.querySelector('.swiper-container')['swiper'];
+    // tslint:disable-next-line: no-string-literal
+    const swiper = document.querySelector('.swiper-container')['swiper'];
     swiper.slidePrev();
   }
-   
+
   // Change between month/week/day
   changeMode(mode) {
     this.calendar.mode = mode;
   }
-   
+
   // Focus today
   today() {
     this.calendar.currentDate = new Date();
   }
-   
+
   // Selected date reange and hence title changed
   onViewTitleChanged(title) {
     this.viewTitle = title;
   }
-   
+
   // Calendar event was clicked
   async onEventSelected(event) {
     // Use Angular date pipe for conversion
-    let start = formatDate(event.startTime, 'medium', this.locale);
-    let end = formatDate(event.endTime, 'medium', this.locale);
-   
-    
+    const start = formatDate(event.startTime, 'medium', this.locale);
+    const end = formatDate(event.endTime, 'medium', this.locale);
+
+
     const alert = await this.alertCtrl.create({
       header: event.title,
       subHeader: event.desc,
@@ -113,10 +115,10 @@ next() {
     });
     alert.present();
   }
-   
+
   // Time slot was clicked
   onTimeSelected(ev) {
-    let selected = new Date(ev.selectedTime);
+    const selected = new Date(ev.selectedTime);
     this.event.startTime = selected.toISOString();
     selected.setHours(selected.getHours() + 1);
     this.event.endTime = (selected.toISOString());
