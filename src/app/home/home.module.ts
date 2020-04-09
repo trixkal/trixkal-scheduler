56de68fc2a80952cff7f566816b780887836288a
unknown <trixkal@gmail.com>
2020-04-08 23:02:33 -0600

    1

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import {NgCalendarModule} from 'ionic2-calendar';
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
+    NgCalendarModule,
     CommonModule,
     FormsModule,
     IonicModule,
